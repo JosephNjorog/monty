b2c7ee2a6812669d24067ce99a2c4454e97b281b
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-15 18:52:30 +0300

    Update executar_monty.c

--- executar_monty.c
+++ executar_monty.c
@@ -1,14 +1,14 @@
 #include "monty.h"
 
 /**
- * execute_monty - executes an instruction of Monty bytecode
+ * monty_executer - executes an instruction of Monty bytecode
  * @instruction: bytecode instruction
  * @ln: line number
  * @top: pointer to top of the stack
  *
  * Return: void
  */
-void execute_monty(char *instruction, unsigned int ln, stack_t **top)
+void monty_executer(char *instruction, unsigned int ln, stack_t **top)
 {
 	char *opcode;
 	void (*f)(stack_t **, unsigned int);
