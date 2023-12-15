80a58254bd7e0dca40c4ef691824059dd814e920
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-15 15:34:09 +0300

    Create rotater.c

---
+++ rotater.c
@@ -0,0 +1,51 @@
+#include "monty.h"
+
+/**
+ * rotl - rotate the linkedlist one item at a time
+ * @stack: pointer to the top of the stack
+ * @line_number: the position of the opcode in the bytecode
+ */
+void rotl(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node, *popped;
+
+	if (stack && *stack && (*stack)->prev)
+	{
+		popped = pop2(stack, line_number);
+		node = *stack;
+		while (node->prev)
+			node = node->prev;
+		node->prev = popped;
+		popped->next = node;
+		popped->prev = NULL;
+	}
+}
+
+/**
+ * rotr - rotates stack to the bottom
+ * @stack: pointer to top of the stack
+ * @line_number: opcode' line number
+ *
+ * Return: void
+ */
+void rotr(stack_t **stack, __attribute__((unused))unsigned int line_number)
+{
+	stack_t *last;
+
+	if (!stack || !(*stack) || !(*stack)->prev)
+		return;
+
+	/* get last element */
+	last = *stack;
+	while (last && last->prev)
+		last = last->prev;
+
+	/* rotate */
+	last->next->prev = NULL;
+	last->prev = *stack;
+	last->next = NULL;
+	(*stack)->next = last;
+
+	/* change TOS */
+	*stack = last;
+}
