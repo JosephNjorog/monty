075c097ae273a6ecfaa78612bf5742f3b384e39d
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-15 19:20:05 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -42,21 +42,21 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void read_monty(int);
+void reed_monty(int);
 void monty_executer(char *, unsigned int, stack_t **);
 char *pata_opc(char *line);
 void (*pata_ins(char *opcode))(stack_t **, unsigned int);
-void push(stack_t **stack, unsigned int line_number);
+void pusher(stack_t **stack, unsigned int line_number);
 stack_t *add_top(stack_t **stack, int n);
-void pall(stack_t **stack, __attribute__((unused))unsigned int line_number);
+void paller(stack_t **stack, __attribute__((unused))unsigned int line_number);
 ssize_t _getlain(char **, size_t *, int);
-void free_stack(stack_t *stack);
-int _atoi(char *nptr, char **endptr, int base);
-void pop(stack_t **, unsigned int);
+void free_stacker(stack_t *stack);
+int s_atoi(char *nptr, char **endptr, int base);
+void poper(stack_t **, unsigned int);
 void ongeza(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, __attribute__((unused))unsigned int line_number);
-stack_t *pop2(stack_t **stack, unsigned int line_number);
-void swap(stack_t **stack, unsigned int line_number);
+void pinter(stack_t **stack, __attribute__((unused))unsigned int line_number);
+stack_t *poper2(stack_t **stack, unsigned int line_number);
+void swaper(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void ondoa(stack_t **stack, unsigned int line_number);
 void zidisha(stack_t **stack, unsigned int line_number);
@@ -64,16 +64,16 @@ int fill_buf(char *buffer, int i, char **b, size_t *n,
 int fd, size_t size)
 unsigned int hakuna(char *buffer);
 void strprinter(stack_t **stack, unsigned int line_number);
-void rotr(stack_t **stack, unsigned int line_number);
+void roteer(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void divaid(stack_t **stack, unsigned int line_number);
 void modul(stack_t **stack, unsigned int line_number);
 void charprinter(stack_t **stack, unsigned int line_number);
-void rotl(stack_t **stack, unsigned int line_number);
-void stack(stack_t **stack, __attribute__((unused))unsigned int line_number);
-void queue(stack_t **stack, __attribute__((unused))unsigned int line_number);
+void roteel(stack_t **stack, unsigned int line_number);
+void stacker(stack_t **stack, __attribute__((unused))unsigned int line_number);
+void queuer(stack_t **stack, __attribute__((unused))unsigned int line_number);
 stack_t *add_queue(stack_t **stack, int n);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *m_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _soma(char *b, unsigned int size, int fd)
 size_t line_saiz(char *b, int i)
 
