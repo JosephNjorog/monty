bfcd7ac194423ad62d5ffe02d8c622a955e8cea4
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-15 19:12:34 +0300

    Update reed_monty.c

--- reed_monty.c
+++ reed_monty.c
@@ -1,12 +1,12 @@
 #include "monty.h"
 
 /**
- * read_monty - reads the monty bytescode instructions to execute
+ * reed_monty - reads the monty bytescode instructions to execute
  * @fd: file desciptor for file containing Monty Bytescode
  *
  * Return: void
  */
-void read_monty(int fd)
+void reed_monty(int fd)
 {
 	char *buffer;
 	size_t i;
@@ -20,7 +20,7 @@ void read_monty(int fd)
 	line_number = 1;
 	while (_getline(&buffer, &i, fd) != -1)
 	{
-		execute_monty(buffer, line_number, &top);
+		monty_executer(buffer, line_number, &top);
 		line_number++;
 	}
 
