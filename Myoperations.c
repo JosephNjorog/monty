9040eff769c9d7253c4774aa7904ca09085f1ab6
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-15 18:45:53 +0300

    Update Myoperations.c

--- Myoperations.c
+++ Myoperations.c
@@ -1,18 +1,18 @@
 #include "monty.h"
 
 /**
- * add - adds top two elements of stack
+ * ongeza - adds top two elements of stack
  * @stack: pointer to top of the stack
  * @line_number: opcode' line number
  *
  * Return: void
  */
-void add(stack_t **stack, unsigned int line_number)
+void ongeza(stack_t **stack, unsigned int line_number)
 {
 	/* check if stack has less than 2 elements */
 	if (stack && (!(*stack) || !(*stack)->prev))
 	{
-		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't ongeza, stack too short\n", line_number);
 		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
@@ -23,18 +23,18 @@ void add(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * sub - subtracts top two elements of stack (2TOS - TOS)
+ * ondoa - subtracts top two elements of stack (2TOS - TOS)
  * @stack: pointer to top of the stack
  * @line_number: opcode' line number
  *
  * Return: void
  */
-void sub(stack_t **stack, unsigned int line_number)
+void ondoa(stack_t **stack, unsigned int line_number)
 {
 	/* check if stack has less than 2 elements */
 	if (stack && (!(*stack) || !(*stack)->prev))
 	{
-		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't ondoa, stack too short\n", line_number);
 		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
@@ -45,18 +45,18 @@ void sub(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * mul - multiplies top two elements of stack
+ * zidisha - multiplies top two elements of stack
  * @stack: pointer to top of the stack
  * @line_number: opcode' line number
  *
  * Return: void
  */
-void mul(stack_t **stack, unsigned int line_number)
+void zidisha(stack_t **stack, unsigned int line_number)
 {
 	/* check if stack has less than 2 elements */
 	if (stack && (!(*stack) || !(*stack)->prev))
 	{
-		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't zidisha, stack too short\n", line_number);
 		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
@@ -67,13 +67,13 @@ void mul(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * divide - perform a division from elements on the stack
+ * divaid - perform a division from elements on the stack
  * @stack: pointer to the top of the stack
  * @line_number: the line number of opcode in bytecode file
  *
  * The format will be 1/TOS * Previous TOS
  */
-void divide(stack_t **stack, unsigned int line_number)
+void divaid(stack_t **stack, unsigned int line_number)
 {
 	stack_t *top;
 
@@ -90,19 +90,19 @@ void divide(stack_t **stack, unsigned int line_number)
 	}
 	else
 	{
-		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't divaid, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 }
 
 /**
- * modulus - return modulus of division
+ * modul - return modulus of division
  * @stack: pointer to the top of the stack
  * @line_number: the line number of opcode in bytecode file
  *
  * The format will be 1/TOS % Previous TOS
  */
-void modulus(stack_t **stack, unsigned int line_number)
+void modul(stack_t **stack, unsigned int line_number)
 {
 	stack_t *top;
 
