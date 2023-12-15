c8e2023bac91dae23aa28ef749b1297d52224d1f
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-15 15:26:19 +0300

    Create executar_monty.c

---
+++ executar_monty.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * execute_monty - executes an instruction of Monty bytecode
+ * @instruction: bytecode instruction
+ * @ln: line number
+ * @top: pointer to top of the stack
+ *
+ * Return: void
+ */
+void execute_monty(char *instruction, unsigned int ln, stack_t **top)
+{
+	char *opcode;
+	void (*f)(stack_t **, unsigned int);
+
+	opcode = get_opcode(instruction);
+	if (opcode && opcode[0] != '#')
+	{
+		f = get_instruction(opcode);
+		if (!f)
+		{
+			fprintf(stderr, "L%u: unknown instruction %s\n", ln, opcode);
+			exit(EXIT_FAILURE);
+		}
+		/* execute bytecode instruction */
+		f(top, ln);
+	}
+}
