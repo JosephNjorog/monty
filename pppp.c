d1843e0b0eb58e9020ea95ad0d26efb079f60945
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-16 10:05:08 +0300

    Update pppp.c

--- pppp.c
+++ pppp.c
@@ -1,17 +1,17 @@
 #include "monty.h"
 
-void monty_pusher(stack_t **stack, unsigned int line_number);
+void monty_push(stack_t **stack, unsigned int line_number);
 void monty_paller(stack_t **stack, unsigned int line_number);
 void monty_pinter(stack_t **stack, unsigned int line_number);
 void monty_poper(stack_t **stack, unsigned int line_number);
 void monty_swaper(stack_t **stack, unsigned int line_number);
 
 /**
- * monty_pusher - Pushes a value to a stack_t linked list.
+ * monty_push - Pushes a value to a stack_t linked list.
  * @stack: A pointer to the top mode node of a stack_t linked list.
  * @line_number: The current working line number of a Monty bytecodes file.
  */
-void monty_pusher(stack_t **stack, unsigned int line_number)
+void monty_push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp, *new;
 	int i;
