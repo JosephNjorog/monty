4bd7907781c0183c16a8c1a8a1099822ddde4547
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-16 14:52:47 +0300

    Update sttok.c

--- sttok.c
+++ sttok.c
@@ -1,20 +1,23 @@
 #include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * set_tokn_error - Sets last element of op_toks to be an error code.
+ * set_op_tok_error - Sets the last element of op_toks to be an error code.
  * @error_code: Integer to store as a string in op_toks.
  */
-void set_tokn_error(int error_code)
+void set_op_tok_error(int error_code)
 {
     int toks_len = 0, i = 0;
     char *exit_str = NULL;
     char **new_toks = NULL;
 
-    toks_len = tokn_arg_len();
+    toks_len = token_arr_len();
     new_toks = malloc(sizeof(char *) * (toks_len + 2));
     if (!op_toks)
     {
-        error_maller();
+        malloc_error();
         return;
     }
     while (i < toks_len)
@@ -22,13 +25,18 @@ void set_tokn_error(int error_code)
         new_toks[i] = op_toks[i];
         i++;
     }
-    exit_str = *get_inter(1, error_code);
+
+    char error_str[20]; // Adjust the size as needed
+    snprintf(error_str, sizeof(error_str), "%d", error_code);
+
+    exit_str = get_inter(strlen(error_str), /* pass the correct file pointer here */);
     if (!exit_str)
     {
         free(new_toks);
-        error_maller();
+        malloc_error();
         return;
     }
+
     new_toks[i++] = exit_str;
     new_toks[i] = NULL;
     free(op_toks);
