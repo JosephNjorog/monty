38bbfa9f699f21d36feb92233f8f143d329f7445
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-16 11:19:18 +0300

    Create sttok.c

---
+++ sttok.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+/**
+ * set_tokn_error - Sets last element of op_toks to be an error code.
+ * @error_code: Integer to store as a string in op_toks.
+ */
+void set_tokn_error(int error_code)
+{
+	int toks_len = 0, i = 0;
+	char *exit_str = NULL;
+	char **new_toks = NULL;
+
+	toks_len = token_arr_len();
+	new_toks = malloc(sizeof(char *) * (toks_len + 2));
+	if (!op_toks)
+	{
+	  error_maller();
+		return;
+	}
+	while (i < toks_len)
+	{
+		new_toks[i] = op_toks[i];
+		i++;
+	}
+	exit_str = get_inter(error_code);
+	if (!exit_str)
+	{
+		free(new_toks);
+		error_maller();
+		return;
+	}
+	new_toks[i++] = exit_str;
+	new_toks[i] = NULL;
+	free(op_toks);
+	op_toks = new_toks;
+}
