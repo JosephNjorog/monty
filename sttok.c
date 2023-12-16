498277a9ba5af086cb8cd498219c1a998aff4a3b
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-16 12:42:43 +0300

    Update sttok.c

--- sttok.c
+++ sttok.c
@@ -1,36 +1,36 @@
 #include "monty.h"
 
 /**
- * set_tokn_error - Sets last element of op_toks to be an error code.
+ * set_tokn_error - Sets the last element of op_toks to be an error code.
  * @error_code: Integer to store as a string in op_toks.
  */
 void set_tokn_error(int error_code)
 {
-	int toks_len = 0, i = 0;
-	char *exit_str = NULL;
-	char **new_toks = NULL;
+    int toks_len = 0, i = 0;
+    char *exit_str = NULL;
+    char **new_toks = NULL;
 
-	toks_len = tokn_arg_len();
-	new_toks = malloc(sizeof(char *) * (toks_len + 2));
-	if (!op_toks)
-	{
-	  error_maller();
-		return;
-	}
-	while (i < toks_len)
-	{
-		new_toks[i] = op_toks[i];
-		i++;
-	}
-	exit_str = get_inter(error_code);
-	if (!exit_str)
-	{
-		free(new_toks);
-		error_maller();
-		return;
-	}
-	new_toks[i++] = exit_str;
-	new_toks[i] = NULL;
-	free(op_toks);
-	op_toks = new_toks;
+    toks_len = tokn_arg_len();
+    new_toks = malloc(sizeof(char *) * (toks_len + 2));
+    if (!op_toks)
+    {
+        error_maller();
+        return;
+    }
+    while (i < toks_len)
+    {
+        new_toks[i] = op_toks[i];
+        i++;
+    }
+    exit_str = get_inter(0, error_code); // Provide len parameter (0 for example)
+    if (!exit_str)
+    {
+        free(new_toks);
+        error_maller();
+        return;
+    }
+    new_toks[i++] = exit_str;
+    new_toks[i] = NULL;
+    free(op_toks);
+    op_toks = new_toks;
 }
