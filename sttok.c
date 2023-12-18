289cb60904a8b52fc2919bc95bc6a71a1ca8f666
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-18 18:36:08 +0300

    Update sttok.c

--- sttok.c
+++ sttok.c
@@ -27,7 +27,7 @@ void set_tokn_error(int error_code)
         i++;
     }
 
-    exit_str = *get_inter(size_t len, FILE *script_fd);
+    exit_str = char *get_inter(size_t len, FILE *script_fd);
     if (!exit_str)
     {
         free(new_toks);
