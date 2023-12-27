a3981fbc8451df9091f2734b4ddd554756216554
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-28 01:38:35 +0300

    Update  file_nop.c

---
+++ file_nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *nope_jnm-  doesn’t do anything.
+  *@head: stack head
+  *@counter: line_number
+  *Return: no return
+ */
+void nope_jnm(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
