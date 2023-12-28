fb89f2a8146d782035e0b2e8095050cc901821cb
Joseph Njoroge <141281465+JosephNjorog@users.noreply.github.com>
2023-12-28 08:42:14 +0300

    Create file_sub.c

---
+++ file_sub.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+/**
+  *subract_jnm- sustration
+  *@head: stack head
+  *@counter: line_number
+  *Return: no return
+ */
+void subract_jnm(stack_t **head, unsigned int counter)
+{
+	stack_t *aux;
+	int sus, nodes;
+
+	aux = *head;
+	for (nodes = 0; aux != NULL; nodes++)
+		aux = aux->next;
+	if (nodes < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack_jnm(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = *head;
+	sus = aux->next->n - aux->n;
+	aux->next->n = sus;
+	*head = aux->next;
+	free(aux);
+}
